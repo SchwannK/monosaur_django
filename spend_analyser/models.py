3ed5d5309a9fa178feb37348d4179d680afa83ff
SchwannK <schwann.khaw@gmail.com>
2016-11-28 01:51:03 +0000

    Added Analyser 'Transaction' model and transaction list

--- spend_analyser/models.py
+++ spend_analyser/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Transaction(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.FloatField()
+    date = models.DateField()
+    classification = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- spend_analyser/views.py
+++ spend_analyser/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Transaction
 
 # Create your views here.
 
 def spend_analyser(request):
-    return render(request, 'spend_analyser/analytics.html', {})
+    transactions = Transaction.objects.order_by('date')
+    return render(request, 'spend_analyser/transaction_list.html', {'transactions': transactions})
