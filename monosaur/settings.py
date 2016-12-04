9a284c383e4df28b691fb2489618fc11c015766c
SchwannK <schwann.khaw@gmail.com>
2016-12-04 18:22:23 +0000

    Added Category fixture

--- monosaur/settings.py
+++ monosaur/settings.py
@@ -132,3 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FIXTURE_DIRS = (
+   '/speed_analyser/fixtures/',
+)
