61701bfe7aa9a7d65a83fff40236a9bf084a2844
SchwannK <schwann.khaw@gmail.com>
2016-12-08 17:13:08 +0000

    Implemented transaction analysis functionality and chart.js display

--- spend_analyser/models.py
+++ spend_analyser/models.py
@@ -1,18 +1,9 @@
 from django.db import models
 from django.utils import timezone
 from ofxparse import OfxParser
+from subscription_list.models import Subscription
 
 # Create your models here.
-
-class Transaction(models.Model):
-    name = models.CharField(max_length=100)
-    amount = models.FloatField()
-    date = models.DateField()
-    category = models.CharField(max_length=100)
-    subscription = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
@@ -20,10 +11,20 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Transaction(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.FloatField()
+    date = models.DateField()
+    category = models.CharField(max_length=50)
+    subscription = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class Company(models.Model):
     name = models.CharField(max_length=50)
     reference = models.CharField(max_length=100)
-    category = models.ForeignKey(Category)
+    category = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
--- spend_analyser/views.py
+++ spend_analyser/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from ofxparse import OfxParser
 from .models import Transaction, Category, Company
 from subscription_list.models import Subscription
-import io
+import io, operator
+
+DEFAULT_TRANSACTION_CAT = "Other"
+DEFAULT_SUBSCRIPTION_NAME = "-"
 
 # Create your views here.
 
@@ -10,8 +13,12 @@ def spend_analyser(request):
     if request.method == "POST":
         add_transactions_to_db(request.FILES['ofx_file'].file)
 
+    chart_data = process_data()
+    chart_labels = list(list(zip(*chart_data))[0])
+    chart_values = list(list(zip(*chart_data))[1])
+
     transactions = Transaction.objects.order_by('-date')
-    return render(request, 'spend_analyser/transaction_list.html', {'transactions': transactions})
+    return render(request, 'spend_analyser/transaction_list.html', {'transactions': transactions, 'chart_values': chart_values, 'chart_labels': chart_labels,})
 
 def add_transactions_to_db(ofx_file):
 
@@ -20,18 +27,48 @@ def add_transactions_to_db(ofx_file):
 
         for transaction in ofx.account.statement.transactions:
 
-            transaction_cat = "Other"
-            for company in Company.objects.all():
-                if company.reference in transaction.payee:
-                    transaction_cat = company.category
+            try:
+                transaction_cat = DEFAULT_TRANSACTION_CAT
+                for company in Company.objects.all():
+                    if company.reference.upper() in transaction.payee:
+                        transaction_cat = company.category
 
-            subscription_name = "No"
-            for subscription in Subscription.objects.all():
-                if subscription.transaction_reference in transaction.payee:
-                    print("yes!")
-                    subscription_name = subscription.subscription_name
+                subscription_name = DEFAULT_SUBSCRIPTION_NAME
+                for subscription in Subscription.objects.all():
+                    if subscription.transaction_reference.upper() in transaction.payee:
+                        subscription_name = subscription.subscription_name
 
-            newTransaction = Transaction.objects.get_or_create(name=transaction.payee, amount=transaction.amount, date=transaction.date, category=transaction_cat, subscription=subscription_name)
-
+                newTransaction = Transaction.objects.get_or_create(name=transaction.payee, amount=transaction.amount, date=transaction.date, category=transaction_cat, subscription=subscription_name)
+            except:
+                print("Transaction error.")
     except:
         print("File not valid!")
+
+def process_data():
+
+    category_totals = {} # Initialise dictionary which will store totals for each category
+    overall_total = 0
+
+    for category in Category.objects.all():
+        category_totals[category.name] = 0      # Initialise all totals to 0
+
+    for transaction in Transaction.objects.all():
+        category_totals[transaction.category] -= transaction.amount # minus to invert sign
+        overall_total -= transaction.amount
+
+    other_total = category_totals.pop(DEFAULT_TRANSACTION_CAT)
+
+    sorted_categories = sorted(category_totals, key=category_totals.get, reverse=True)[:4]
+    top_total = 0
+    for top_cat in sorted_categories:
+        top_total += category_totals[top_cat]
+
+    sorted_categories.append(DEFAULT_TRANSACTION_CAT)
+    category_totals[DEFAULT_TRANSACTION_CAT] = overall_total - top_total
+
+    chart_data = [(cat, round(category_totals[cat],2)) for cat in sorted_categories]
+
+    # for key, value in chart_data:
+    #    print("%s: %d" % (key, value))
+
+    return chart_data
