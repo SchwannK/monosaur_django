00b520fcc760870257ad7015a0540111b37eaefc
Gabor Biro <gabor.biro@yahoo.com>
2016-12-10 13:14:26 +0000

    Resolved conflits

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -6,17 +6,5 @@ from subscriptions.models import Subscription
 
 def subscriptions(request):
     print("==================== subscriptions ======================")
-<<<<<<< HEAD
-    session_id = get_session_id(request, False)
-    transactions = Transaction.objects\
-        .filter(subscription__isnull=False, user=session_id)\
-        .values('name', 'subscription__name', 'subscription__company__name', \
-                'subscription__company__category__name', 'subscription__description', 'subscription__monthly_price')\
-        .distinct()
-    print("Result: " + str(transactions))
-    return render(request, 'subscriptions/subscriptions.html', {'navbar':'subscriptions', 'subscriptions': transactions})
-=======
     subscriptions = Subscription.objects.order_by('company__name')
-    return render(request, 'subscriptions/subscriptions.html', {'subscriptions': subscriptions})
-
->>>>>>> origin/master
+    return render(request, 'subscriptions/subscriptions.html', {'navbar':'subscriptions', 'subscriptions': subscriptions})
