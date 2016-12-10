d2b337852186bdfc4538957ff53eef5da4848008
SchwannK <schwann.khaw@gmail.com>
2016-12-10 12:43:05 +0000

    Fixed Chartjs display and added checks for transaction data in html

--- spend_analyser/views.py
+++ spend_analyser/views.py
@@ -13,45 +13,51 @@ from .transactions.transactions import save_transactions
 def spend_analyser(request):
     print("==================== spend_analyser ======================")
     session_id = get_session_id(request, False)
-    
+
     if request.method == "POST":
         transactions = read_transactions(request.FILES['ofx_file'].file)
         save_transactions(transactions, session_id)
     transactions = Transaction.objects.filter(user=session_id).order_by('-date')
-    
-    chart_data = get_chart(Category.objects.all(), Transaction.objects.all())
-    chart_labels = list(list(zip(*chart_data))[0])
-    chart_values = list(list(zip(*chart_data))[1])
-    print("Result: " + str(len(transactions)))
-    return render(request, 'spend_analyser/transaction_list.html', {'transactions': transactions, 'chart_values': chart_values, 'chart_labels': chart_labels, })
 
+    # If transactions exist, perform analysis and show chart
+    if Transaction.objects.all().count() > 0:
+        chart_data = get_chart(Category.objects.all(), Transaction.objects.all())
+        chart_labels = list(list(zip(*chart_data))[0])
+        chart_values = list(list(zip(*chart_data))[1])
+    # print("Result: " + str(len(transactions)))
+        return render(request, 'spend_analyser/transaction_list.html', {'transactions': transactions, 'chart_values': chart_values, 'chart_labels': chart_labels, })
+
+    # If no transactions exist, don't show anything
+    else:
+        return render(request, 'spend_analyser/transaction_list.html', {'transactions': transactions,})
 
 def get_chart(categories, transactions):
     category_totals = {}  # Initialise dictionary which will store totals for each category
     overall_total = 0
 
-    for category in categories:
-        category_totals[category.name] = 0  # Initialise all totals to 0
-
     for transaction in transactions:
-        category_totals[transaction.category] = category_totals.get(transaction.category, 0) - transaction.amount  # minus to invert sign
+        category_totals[transaction.category.name] = category_totals.get(transaction.category.name, 0) - transaction.amount  # minus to invert sign
         overall_total -= transaction.amount
 
-#     other_total = category_totals.pop(DEFAULT_TRANSACTION_CAT)
+    for key in category_totals:
+        print("%s: %d" % (key, category_totals[key]))
 
-    sorted_categories = sorted(category_totals, key=category_totals.get, reverse=True)[:4]
+    # Remove Other category to find top 4 non-Other categories
+    other_total = category_totals.pop(DEFAULT_TRANSACTION_CATEGORY)
+
+    print(other_total)
+
+    sorted_categories = sorted(category_totals, key=category_totals.get, reverse=True)[:4] # get top 4 non-Other categories in sorted list
+
+    # Calculate total spend in top 4 non-Other categories
     top_total = 0
     for top_cat in sorted_categories:
         top_total += category_totals[top_cat]
 
+    # Add back Other category and recalculate to include all categories not within top 4
     sorted_categories.append(DEFAULT_TRANSACTION_CATEGORY)
-    
-    if DEFAULT_TRANSACTION_CATEGORY in category_totals:
-        category_totals[DEFAULT_TRANSACTION_CATEGORY] = overall_total - top_total
+    category_totals[DEFAULT_TRANSACTION_CATEGORY] = overall_total - top_total
 
     chart_data = [(cat, round(category_totals.get(cat, 0), 2)) for cat in sorted_categories]
-
-    # for key, value in chart_data:
-    #    print("%s: %d" % (key, value))
 
     return chart_data
