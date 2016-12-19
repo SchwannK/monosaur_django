7adaac18010c8c401e2b17f3c39e73dbbc00a784
Gabor Biro <gabor.biro@yahoo.com>
2016-12-19 19:51:18 +0000

    * Attempting to fix server file path issue

--- monosaur/utils/fixture_utils.py
+++ monosaur/utils/fixture_utils.py
@@ -6,7 +6,9 @@ from django.core.management import call_command
 # dump the specified database table into a json file
 def create_fixture(model, path):
     sysout = sys.stdout
+    print("os.path.dirname(__name__): " + str(os.path.dirname(__name__)))
     my_dir = os.path.abspath(os.path.dirname(__name__))
+    print("my_dir: " + str(my_dir))
     f = open(os.path.join(my_dir, path).replace('\\', '/'), 'w+')
     sys.stdout = f
     call_command('dump_object', model, '*')
