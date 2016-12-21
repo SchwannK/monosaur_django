a4e4ddae5d4311b7b31b00a8ff03e53cb2e2adb6
Gabor Biro <gabor.biro@yahoo.com>
2016-12-21 10:40:12 +0000

    Fixed issues with db_cleaner

--- monosaur/management/commands/clean_db.py
+++
@@ -1,7 +0,0 @@
-from django.core.management.base import BaseCommand
-from spend_analyser.transactions import transaction_handler
-
-class Command(BaseCommand):
-    
-    def handle(self, *args, **options):
-        transaction_handler.delete_old_entries()
